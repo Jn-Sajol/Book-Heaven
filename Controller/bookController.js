e1ca76477b182e25e5903f64817499f3f6362823
Jn_Sajol <julkar10121@gmail.com>
2025-01-14 13:53:41 +0600

    books api creating

--- Controller/bookController.js
+++ Controller/bookController.js
@@ -1,3 +1,4 @@
+const booksModel = require("../Models/booksModel");
 const userModel = require("../Models/userModel");
 
 const addWhiteList = async (req, res) => {
@@ -56,5 +57,29 @@ const seeWhiteList = async (req, res) => {
 //Comment for specific books
 
 const createComment = async (req,res) => {
-    
+    try {
+        const id = req.user.id;
+    const comment = req.body;
+    const bookId = req.params;
+    const user = await userModel.findOne({ id });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user cant find",
+      });
+    }
+
+    const book = await booksModel.findOne(bookId);
+    book.comment.push(comment)
+    res.status(200).json({
+        success: true,
+        message: "book added in White list",
+      });
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'Server Error',
+        })
+    }
 }
