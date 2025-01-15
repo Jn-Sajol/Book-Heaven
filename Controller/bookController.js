ccf081b7db0e8910f6c668cb1aab2842ee191e32
Jn_Sajol <julkar10121@gmail.com>
2025-01-15 12:07:39 +0600

    get all comments from specific books

--- Controller/bookController.js
+++ Controller/bookController.js
@@ -1,15 +1,15 @@
 const booksModel = require("../Models/booksModel");
 const userModel = require("../Models/userModel");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 //create book
 const createBook = async (req, res) => {
   try {
-    const { name,author,description } = req.body;
-    console.log(name)
+    const { name, author, description } = req.body;
+    console.log(name);
 
     //check existency
-    const existency = await booksModel.findOne({name});
+    const existency = await booksModel.findOne({ name });
     if (existency || existency < 0) {
       return res.status(404).json({
         success: false,
@@ -36,17 +36,17 @@ const createBook = async (req, res) => {
 };
 
 //Get Books List
-const bookList = async (req,res) =>{
- try {
-  const bookList = await booksModel.find();
-  res.send(bookList)
- } catch (error) {
-  res.status(500).json({
-    success: false,
-    message: `server error ${error}`,
-  });
- }
-}
+const bookList = async (req, res) => {
+  try {
+    const bookList = await booksModel.find();
+    res.send(bookList);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `server error ${error}`,
+    });
+  }
+};
 
 //Add whitelist
 const addWhiteList = async (req, res) => {
@@ -54,10 +54,10 @@ const addWhiteList = async (req, res) => {
     const bookId = req.params.id;
     const id = req.user.userId;
 
-    console.log('bookid =>',bookId, 'user id =>', id)
+    console.log("bookid =>", bookId, "user id =>", id);
 
-    const user = await userModel.findOne({_id: id});
-    console.log('user found',user)
+    const user = await userModel.findOne({ _id: id });
+    console.log("user found", user);
     if (!user) {
       return res.status(404).json({
         success: false,
@@ -65,20 +65,24 @@ const addWhiteList = async (req, res) => {
       });
     }
 
-    if (user.whitelist.some((book) => book?.bookId?.toString() === bookId.toString())) {
+    if (
+      user.whitelist.some(
+        (book) => book?.bookId?.toString() === bookId.toString()
+      )
+    ) {
       return res.status(404).json({
         success: false,
         message: "This book alresy exist in white list",
       });
     }
 
-    user.whitelist.push({bookId:bookId});
+    user.whitelist.push({ bookId: bookId });
     await user.save();
     console.log("Whitelist updated:", user.whitelist);
     res.status(200).json({
       success: true,
       message: "book added in White list",
-      whitelist: user.whitelist
+      whitelist: user.whitelist,
     });
   } catch (error) {
     return res.status(500).json({
@@ -91,9 +95,9 @@ const addWhiteList = async (req, res) => {
 //     try {
 //       const bookId = req.params.id; // Extract bookId from params
 //       const id = req.user.userId; // Extract userId from request
-  
+
 //       console.log("bookId =>", bookId, "userId =>", id);
-  
+
 //       // Validate and convert bookId to ObjectId
 //     //   if (!mongoose.Types.ObjectId.isValid(bookId)) {
 //     //     return res.status(400).json({
@@ -102,7 +106,7 @@ const addWhiteList = async (req, res) => {
 //     //     });
 //     //   }
 //     //   const objectIdBookId = new mongoose.Types.ObjectId(bookId);
-  
+
 //       // Find the user
 //       const user = await userModel.findOne({ _id: id });
 //       if (!user) {
@@ -111,7 +115,7 @@ const addWhiteList = async (req, res) => {
 //           message: "User not found",
 //         });
 //       }
-  
+
 //       // Check if the bookId already exists in the whitelist
 //       if (user.whitelist.some((book) => book?.bookId?.toString() === bookId.toString())) {
 //         return res.status(400).json({
@@ -119,11 +123,11 @@ const addWhiteList = async (req, res) => {
 //           message: "This book already exists in the whitelist",
 //         });
 //       }
-  
+
 //       // Add the bookId to the whitelist
 //       user.whitelist.push({ bookId: bookId });
 //       await user.save();
-  
+
 //       console.log("Whitelist updated:", user.whitelist);
 //       res.status(200).json({
 //         success: true,
@@ -138,15 +142,12 @@ const addWhiteList = async (req, res) => {
 //       });
 //     }
 //   };
-  
-  
-  
 
 //See All whitelist
 const seeWhiteList = async (req, res) => {
   try {
     const id = req.user.userId;
-    console.log(id)
+    console.log(id);
     const user = await userModel.findOne({ _id: id });
 
     if (!user) {
@@ -168,45 +169,66 @@ const seeWhiteList = async (req, res) => {
   }
 };
 
-
 //Comment for specific books
 const addComment = async (req, res) => {
   try {
-      const userId = req.user.userId; // Assuming user ID is coming from authentication middleware
-      const { comment } = req.body;
-      const bookId = req.params.id;
+    const userId = req.user.userId; // Assuming user ID is coming from authentication middleware
+    const { comment } = req.body;
+    const bookId = req.params.id;
 
-      // Find the book by ID
-      const book = await booksModel.findById(bookId);
-      if (!book) {
-          return res.status(404).json({ success: false, message: 'Book not found' });
-      }
+    // Find the book by ID
+    const book = await booksModel.findById(bookId);
+    if (!book) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Book not found" });
+    }
 
-      // Find the user by ID to get the username
-      const user = await userModel.findById(userId);
-      if (!user) {
-          return res.status(404).json({ success: false, message: 'User not found' });
-      }
+    // Find the user by ID to get the username
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
 
-      // Push the comment with username into the book's comment array
-      book.comment.push({ userId: user._id, comment, username: user.username });
-      await book.save();
+    // Push the comment with username into the book's comment array
+    book.comment.push({ userId: user._id, comment, username: user.username });
+    await book.save();
 
-      res.status(200).json({
-          success: true,
-          message: 'Comment added successfully',
-          book,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Comment added successfully",
+      book,
+    });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: 'Server Error' });
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 };
 
-
+//const get all comments with user name
+const getAllcomments = async (req, res) => {
+  try {
+    const bookid = req.params.id;
+    const allcomments = await booksModel.findById(bookid)
+    res.send(allcomments.comment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
 //privet route
-const privetRoute = (req,res) =>{
-    res.send('welcome to privet route')
-  }
-module.exports = {createBook, bookList, addWhiteList, seeWhiteList, addComment ,privetRoute};
+const privetRoute = (req, res) => {
+  res.send("welcome to privet route");
+};
+module.exports = {
+  createBook,
+  bookList,
+  addWhiteList,
+  seeWhiteList,
+  addComment,
+  privetRoute,
+  getAllcomments
+};
--- Router/bookRoute.js
+++ Router/bookRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addWhiteList, createBook,bookList, seeWhiteList ,addComment} = require('../Controller/bookController');
+const { addWhiteList, createBook,bookList, seeWhiteList ,addComment,getAllcomments} = require('../Controller/bookController');
 const {userAuth} = require('../Middleware/userAuth');
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/create',createBook)
 router.get('/booklist',bookList)
 router.get('/seewhitelist',userAuth,seeWhiteList)
 router.post('/addcomment/:id',userAuth,addComment)
+router.get('/allcomments/:id',getAllcomments)
 
 module.exports = router;
