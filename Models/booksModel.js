7d0cfe0b2c2e85abe2029a400be1bd14bb615cba
Jn_Sajol <julkar10121@gmail.com>
2025-01-20 23:39:43 +0600

    delete book added

--- Models/booksModel.js
+++ Models/booksModel.js
@@ -1,19 +1,55 @@
+// const mongoose = require('mongoose');
+
+// // books scheema
+// const bookScheema = new mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     author:{
+//         type:String,
+//         required:true
+//     },
+//     description:{
+//         type:String
+//     },
+//     publisDate:{type:Date,default:Date.now},
+    
+// })
+// module.exports = mongoose.model('Books',bookScheema)
+
 const mongoose = require('mongoose');
 
-// books scheema
-const bookScheema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    author:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String
-    },
-    publisDate:{type:Date,default:Date.now},
-    
-})
-module.exports = mongoose.model('Books',bookScheema)
+// Books schema
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  publishDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Middleware for cascading delete
+bookSchema.pre('deleteOne', { document: false, query: true }, async function (next) {
+  const bookId = this.getQuery()['_id'];
+
+  try {
+    // Delete all Whitelist entries referencing the book
+    await mongoose.model('Whitelist').deleteMany({ bookId });
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model('Books', bookSchema);
--- Models/whiteListModel.js
+++ Models/whiteListModel.js
@@ -1,13 +1,31 @@
+// const mongoose = require('mongoose');
+
+// const whitelistShema = new mongoose.Schema({
+//     bookId:{
+//         type:mongoose.Schema.Types.ObjectId,
+//         ref:'Books',
+//         index: true
+//     },
+//     userId:{
+//         type:mongoose.Schema.Types.ObjectId,
+//         ref:"User"
+//     }
+// });
+// module.exports = mongoose.model('Whitelist',whitelistShema);
+
 const mongoose = require('mongoose');
 
-const whitelistShema = new mongoose.Schema({
-    bookId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Books'
-    },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    }
+// Whitelist schema
+const whitelistSchema = new mongoose.Schema({
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Books',
+    index: true, // Indexing for better performance
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
-module.exports = mongoose.model('Whitelist',whitelistShema);
+
+module.exports = mongoose.model('Whitelist', whitelistSchema);
